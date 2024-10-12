b380503df8805778b61a7f276d47128127e23bb3
dannycahyo <dannydwicahyono@gmail.com>
2024-10-12 11:49:34 +0700

    feat: initialize db connection to postgres with pg

---
+++ src/db.ts
@@ -0,0 +1,12 @@
+import { Client } from "pg";
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+});
+
+client
+  .connect()
+  .then(() => console.log("Connected to PostgreSQL"))
+  .catch((err) => console.error("Connection error", err.stack));
+
+export default client;
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
 import { Hono } from "hono";
 import transactionsRouter from "./routes/transactions";
 import categoriesRouter from "./routes/categories";
+import client from "./db";
 
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.text("Welcome to Fin Track API");
+});
+
+app.get("/test-db", async (c) => {
+  try {
+    const res = await client.query("SELECT NOW()");
+    return c.json({
+      message: "Connected to PostgreSQL",
+      time: res.rows[0].now,
+    });
+  } catch (err) {
+    return c.json(
+      {
+        message: "Failed to connect to PostgreSQL",
+        error: (err as Error).message,
+      },
+      500,
+    );
+  }
 });
 
 app.route("/transactions", transactionsRouter);
