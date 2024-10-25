2dde62d288aeb0dfee63cbb12bce97c7ec488a97
dannycahyo <dannydwicahyono@gmail.com>
2024-10-25 18:34:38 +0700

    feat: Add logging configuration and add logger module
    - Update index.ts to include a middleware for logging request durations
    - Add logger module to handle logging using Winston library
    - Create logs directory and app.log file for storing logs
    - Update categories.ts to include error logging for GET, POST, PATCH, and DELETE requests

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import { Hono } from "hono";
 import transactionsRouter from "./routes/transactions";
 import categoriesRouter from "./routes/categories";
+import logger from "./logger";
 
 const app = new Hono();
 
+app.use("*", async (c, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  logger.info(`${c.req.method} ${c.req.url} - ${ms}ms`);
+});
+
 app.get("/", (c) => {
+  logger.info("GET /");
   return c.text("Welcome to Fin Track API");
 });
 
--- src/routes/categories.ts
+++ src/routes/categories.ts
@@ -1,6 +1,8 @@
 import { Hono } from "hono";
 import * as categoryRepo from "../repository/categoryRepo";
 import { CategorySchema } from "../types/Category";
+import logger from "../logger";
+
 import type { Category } from "../types/Category";
 
 const categoriesRouter = new Hono();
@@ -11,10 +13,23 @@ categoriesRouter.get("/", async (c) => {
 });
 
 categoriesRouter.get("/:id", async (c) => {
-  const category = await categoryRepo.getCategoryById(
-    c.req.param("id"),
-  );
-  return category ? c.json({ data: category }) : c.notFound();
+  try {
+    const category = await categoryRepo.getCategoryById(
+      c.req.param("id"),
+    );
+    return category ? c.json({ data: category }) : c.notFound();
+  } catch (error) {
+    if (error instanceof Error) {
+      logger.error(
+        `GET /categories/${c.req.param("id")} - ${error.message}`,
+      );
+      return c.json({ error: error.message }, 400);
+    }
+    logger.error(
+      `GET /categories/${c.req.param("id")} - Unknown error`,
+    );
+    return c.json({ error: "Unknown error" }, 400);
+  }
 });
 
 categoriesRouter.post("/", async (c) => {
@@ -28,8 +43,10 @@ categoriesRouter.post("/", async (c) => {
     return c.json({ data: createdCategory }, 201);
   } catch (error) {
     if (error instanceof Error) {
+      logger.error(`POST /categories - ${error.message}`);
       return c.json({ error: error.message }, 400);
     }
+    logger.error(`POST /categories - Unknown error`);
     return c.json({ error: "Unknown error" }, 400);
   }
 });
@@ -48,16 +65,31 @@ categoriesRouter.patch("/:id", async (c) => {
       : c.json({ error: "Category not found" }, 422);
   } catch (error) {
     if (error instanceof Error) {
+      logger.error(`PATCH /categories/${id} - ${error.message}`);
       return c.json({ error: error.message }, 400);
     }
+    logger.error(`PATCH /categories/${id} - Unknown error`);
     return c.json({ error: "Unknown error" }, 400);
   }
 });
 
 categoriesRouter.delete("/:id", async (c) => {
-  const id = c.req.param("id");
-  const category = await categoryRepo.deleteCategory(id);
-  return category ? c.text("Category deleted", 204) : c.notFound();
+  try {
+    const id = c.req.param("id");
+    const category = await categoryRepo.deleteCategory(id);
+    return category ? c.text("Category deleted", 204) : c.notFound();
+  } catch (error) {
+    if (error instanceof Error) {
+      logger.error(
+        `DELETE /categories/${c.req.param("id")} - ${error.message}`,
+      );
+      return c.json({ error: error.message }, 400);
+    }
+    logger.error(
+      `DELETE /categories/${c.req.param("id")} - Unknown error`,
+    );
+    return c.json({ error: "Unknown error" }, 400);
+  }
 });
 
 export default categoriesRouter;
